3e6e241205a35fc7cc940e9ab1e7f59c39fb8c52
Andres Blanco Coto <andblanco@estudiantec.cr>
2023-10-17 22:28:45 -0600

    Se corrigieron varios errores en el codigo

--- src/main/java/com/EstructurasDatos/MainArbol.java
+++ src/main/java/com/EstructurasDatos/MainArbol.java
@@ -1,75 +1,76 @@
 package com.EstructurasDatos;
 import java.util.Scanner;
-
 class Nodo {
     String valor;
     Nodo izquierdo, derecho;
-
     Nodo(String item) {
         valor = item;
         izquierdo = derecho = null;
     }
 }
-
 class ArbolBinario {
     Nodo raiz;
-
     ArbolBinario() {
         raiz = null;
     }
-
     void insertar(String valor) {
         raiz = insertarRec(raiz, valor);
     }
-
     Nodo insertarRec(Nodo raiz, String valor) {
         if (raiz == null) {
             raiz = new Nodo(valor);
             return raiz;
         }
-
         if (esOperador(valor)) {
             raiz.izquierdo = insertarRec(raiz.izquierdo, valor);
         } else {
             raiz.derecho = insertarRec(raiz.derecho, valor);
         }
-
         return raiz;
     }
-
     boolean esOperador(String valor) {
         return valor.equals("+") || valor.equals("-") || valor.equals("*") || valor.equals("/");
     }
-
-    void inorden() {
-        inordenRec(raiz);
+    double evaluar() {
+        return evaluarRec(raiz);
     }
-
-    void inordenRec(Nodo raiz) {
-        if (raiz != null) {
-            inordenRec(raiz.izquierdo);
-            System.out.print(raiz.valor + " ");
-            inordenRec(raiz.derecho);
+    double evaluarRec(Nodo raiz) {
+        if (raiz == null) {
+            return 0;
+        }
+        if (!esOperador(raiz.valor)) {
+            return Double.parseDouble(raiz.valor);
+        }
+        double izquierdo = evaluarRec(raiz.izquierdo);
+        double derecho = evaluarRec(raiz.derecho);
+        switch (raiz.valor) {
+            case "+":
+                return izquierdo + derecho;
+            case "-":
+                return izquierdo - derecho;
+            case "*":
+                return izquierdo * derecho;
+            case "/":
+                return izquierdo / derecho;
+            default:
+                return 0;
         }
     }
 }
-
 public class MainArbol {
     public static void main(String[] args) {
         ArbolBinario arbol = new ArbolBinario();
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Ingrese la expresión matemática en notación de árbol binario:");
         String expresion = scanner.nextLine();
-
         String[] valores = expresion.split(" ");
         for (String valor : valores) {
-            arbol.insertar(valor);
+            if (valor != null && !valor.isEmpty()) {
+                arbol.insertar(valor);
+            }
         }
-
-        System.out.println("Recorrido en orden del árbol:");
-        arbol.inorden();
-
+        double resultado = arbol.evaluar();
+        System.out.println("El resultado de la operación es: " + resultado);
         scanner.close();
     }
 }
