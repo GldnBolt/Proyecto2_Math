8e3b3704fb9a7f05bf610b606f89dfcbcf4e30c3
Andres Blanco Coto <andblanco@estudiantec.cr>
2023-10-24 00:49:24 -0600

    Se corrigio el codigo de ExpresionesLogicas.java

--- src/main/java/com/EstructurasDatos/ExpresionesLogicas.java
+++ src/main/java/com/EstructurasDatos/ExpresionesLogicas.java
@@ -12,25 +12,17 @@ public class ExpresionesLogicas {
     }
 
     public static String convertirExpresionesLogicas(String expresion) {
-        // Expresiones regulares para identificar las expresiones lógicas
-        String regexAnd = "\\b&&\\b";
-        String regexOr = "\\b\\|\\|\\b";
-        String regexNot = "\\b!\\b";
-        String regexXor = "\\b\\^\\b";
-
-        // Variantes algebraicas de las expresiones lógicas
-        String algebraicoAnd = "*";
-        String algebraicoOr = "+";
-        String algebraicoNot = "-";
-        String algebraicoXor = "xor";
-
         // Reemplazar las expresiones lógicas por sus variantes algebraicas
+        String resultado = expresion.replaceAll("&&", "*")
+                .replaceAll("\\|\\|", "+")
+                .replaceAll("!", "~")
+                .replaceAll("\\^", "xor");
 
-        return expresion.replaceAll(regexAnd, algebraicoAnd)
-                .replaceAll(regexOr, algebraicoOr)
-                .replaceAll(regexNot, algebraicoNot)
-                .replaceAll(regexXor, algebraicoXor);
+        return resultado;
     }
 }
+
+
+
 
 
